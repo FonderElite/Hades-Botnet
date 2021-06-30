e7a81967fcdb78fc3319871a2c5313383feacecf
FonderElite <s1170044@usls.edu.ph>
2021-06-30 14:35:54 +0000

    Update hades_commander.py

--- hades_commander.py
+++ hades_commander.py
@@ -1,44 +1,45 @@
-import multiprocessing,os,sys,platform,argparse,time,platform
-from multiprocessing import Process
+import os,sys,time,socket,platform
 from tabulate import tabulate
-from colorama import Fore,init
+from colorama import Fore,init 
 from colorama import Back as bg
-parser = argparse.ArgumentParser(description='BotNet Attacker Main Controller')
-parser.add_argument('-o','--option',metavar='',help='')
-args = parser.parse_args()
 init(autoreset=True)
-class Controller(object):
-    def __init__(self,option):
-        self.option = option
-    @staticmethod
-    def show_banner(s):
-        for c in s + '\n':
-            sys.stdout.write(bg.BLACK + Fore.WHITE + c)
-            sys.stdout.flush()
-            time.sleep(1.2 / 100)
-        time.sleep(1.5)
-        print('-----------------------------------------------------------------')
-        print(Fore.WHITE + '[' + Fore.GREEN + '+' + Fore.WHITE + ']Made By Droid | Github:https://github.com/FonderElite')
-        print('-----------------------------------------------------------------')
-        time.sleep(1)
-    def command(self):
-        try:
-            table = [["Options","Severity"],["Ddos-attack(0)",5],["Backdoor(1)",5],["Auto-Root(2)",5]]
-            print(tabulate(table,headers='firstrow',tablefmt='grid'))
-            if  args.option == 0:
-                print('Proceeding with a DDOS-Attack')
-            elif args.option == None:
-                print('Pass Arguments to python3 <hades-commander.py> -o (number)')
-            else:
-                print('Use python3 <hades-commander.py> -h for help.')
-        except Exception as Err:
-            print(Err)
+def show_banner(s):
+    for c in s + '\n':
+        sys.stdout.write(bg.BLACK + Fore.WHITE + c)
+        sys.stdout.flush()
+        time.sleep(1.2 / 100)
+    time.sleep(1.5)
+    print('-----------------------------------------------------------------')
+    print(Fore.WHITE + '[' + Fore.GREEN + '+' + Fore.WHITE + ']Made By Droid | Github:https://github.com/FonderElite')
+    print('-----------------------------------------------------------------')
+    time.sleep(1)
+def options():
+      table = [["Options","Severity"],["Ddos-attack(0)",5],["Backdoor(1)",5],["Auto-Root(2)",5]]
+      print(tabulate(table,headers='firstrow',tablefmt='grid'))
 
+def execute():
+    s =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    if os.getuid() == 0:
+        global root_fmt
+        root_fmt = '''
+{red}┌──(root💀{host_name})-[{cwd}]
+└─Option# '''.format(host_name=host,cwd=os.getcwd(),red=Fore.RED,white=Fore.WHITE)
+    elif os.getuid() != 0:
+        root_fmt = '''
+{blue}┌──({host_name}㉿Hades)-[{cwd}]
+└─Option$ '''.format(host_name=host,cwd=os.getcwd(),blue=Fore.BLUE,white=Fore.WHITE)
+    print(root_fmt,end='')
+    choice = input("")
+    if choice == "0":
+        ip = input("Server Ip: ")
+        port = int(input("Server Port: "))
+        s.connect((ip, port)) 
+    else:
+        print(f'{Fore.WHITE}[{Fore.GREEN}+{Fore.WHITE}]Invalid Option.')
 
 if __name__ == '__main__':
-    class_object = Controller(args.option)
-    command = Process(target=class_object.command)
-    banner = Process(target=class_object.show_banner,args=('''
+    banner = show_banner('''
 ______________________________________________________________
   ____  
  / ___|___  _ __ ___  _ __ ___   __ _ _ __   __| | ___ _ __ 
@@ -46,8 +47,7 @@ ______________________________________________________________
 | |__| (_) | | | | | | | | | | | (_| | | | | (_| |  __/ |   
  \____\___/|_| |_| |_|_| |_| |_|\__,_|_| |_|\__,_|\___|_|   
 ______________________________________________________________
-        ''',))
-    banner.start()
-    banner.join()
-    command.start()
-    command.join()
+        ''')
+    options()
+    execute()
+                
